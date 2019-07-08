0ea4dcd5c6c9da4618f35a3fbb5c73a29180c71b
Sung hak Jung <hakburi@naver.com>
2019-07-08 11:00:07 +0900

    프로젝트 생성

---
+++ MakeProjectTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeProjectTest
+//
+//  Created by Jung SungHak on 08/07/2019.
+//  Copyright © 2019 Jung SungHak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
